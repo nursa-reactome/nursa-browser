c72c723f9c56ca3364480c63edd0764934b64d2b
FredLoney <loneyf@ohsu.edu>
2018-07-30 10:49:53 -0700

    Override the fireworks hidable container panel.

--- src/main/java/org/reactome/web/nursa/fireworks/client/NursaFireworksCanvas.java
+++ src/main/java/org/reactome/web/nursa/fireworks/client/NursaFireworksCanvas.java
@@ -1,11 +1,12 @@
 package org.reactome.web.nursa.fireworks.client;
 
 import org.reactome.web.fireworks.client.FireworksCanvas;
-import org.reactome.web.fireworks.legends.EnrichmentLegend;
 import org.reactome.web.fireworks.model.Graph;
+import org.reactome.web.nursa.fireworks.controls.settings.NursaHideableContainerPanel;
 import org.reactome.web.nursa.fireworks.legends.NursaEnrichmentLegend;
 
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.user.client.ui.Panel;
 
 public class NursaFireworksCanvas extends FireworksCanvas {
 
@@ -15,7 +16,12 @@ public class NursaFireworksCanvas extends FireworksCanvas {
     }
 
     @Override
-    protected EnrichmentLegend createLegendPanel(EventBus eventBus) {
+    protected Panel createHideablePanel(EventBus eventBus) {
+        return new NursaHideableContainerPanel(eventBus);
+    }
+
+    @Override
+    protected Panel createLegendPanel(EventBus eventBus) {
         return new NursaEnrichmentLegend(eventBus);
     }
 
---
+++ src/main/java/org/reactome/web/nursa/fireworks/controls/settings/NursaHideableContainerPanel.java
@@ -0,0 +1,30 @@
+package org.reactome.web.nursa.fireworks.controls.settings;
+
+import org.reactome.web.fireworks.controls.settings.HideableContainerPanel;
+import org.reactome.web.fireworks.events.AnalysisPerformedEvent;
+import org.reactome.web.fireworks.handlers.AnalysisPerformedHandler;
+import org.reactome.web.fireworks.handlers.AnalysisResetHandler;
+import org.reactome.web.nursa.analysis.client.model.ComparisonExpressionSummary;
+
+import com.google.gwt.event.shared.EventBus;
+
+public class NursaHideableContainerPanel extends HideableContainerPanel
+implements AnalysisPerformedHandler, AnalysisResetHandler{
+
+    public NursaHideableContainerPanel(EventBus eventBus) {
+        super(eventBus);
+    }
+
+    @Override
+    public void onAnalysisReset() {
+        profilesBtn.setVisible(true);
+    }
+
+    @Override
+    public void onAnalysisPerformed(AnalysisPerformedEvent e) {
+        boolean isComparison =
+                e.getExpressionSummary() instanceof ComparisonExpressionSummary;
+        profilesBtn.setVisible(!isComparison);
+    }
+
+}
