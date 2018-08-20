3617bb3ebb08611ef0bcbb30ac0207674997d013
FredLoney <loneyf@ohsu.edu>
2018-08-20 12:31:22 -0700

    Rename comparison data point FDR to PValue.

--- src/main/java/org/reactome/web/nursa/model/ComparisonDataPoint.java
+++ src/main/java/org/reactome/web/nursa/model/ComparisonDataPoint.java
@@ -25,22 +25,22 @@ public class ComparisonDataPoint {
     }
     
     /**
-     * @return the log10(FDR1/FDR2) ratio
+     * @return the log10(pvalue1/pvalue2) ratio
      */
     public Double getRatio() {
         DataPoint dp1 = getDataPoints()[0];
-        Double value1 = getFDR(dp1);
+        Double value1 = getPvalue(dp1);
         DataPoint dp2 = getDataPoints()[1];
-        Double value2 = getFDR(dp2);
+        Double value2 = getPvalue(dp2);
         return value1 == null || value2 == null ? null : Math.log10(value1 / value2);
     }
 
-    protected Double getFDR(DataPoint dp) {
+    protected Double getPvalue(DataPoint dp) {
         if (dp == null) {
              return null;
         }
-        double fdr = dp.getPvalue();
-        return fdr == 0.0 ? Double.MIN_VALUE : fdr;
+        double pValue = dp.getPvalue();
+        return pValue == 0.0 ? Double.MIN_VALUE : pValue;
     }
 
 }
