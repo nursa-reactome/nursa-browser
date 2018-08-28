aade5bf37a0d9621c717a03964873c790edad5b6
FredLoney <loneyf@ohsu.edu>
2018-08-28 10:41:15 -0700

    Add binomial configuration configuration widget.

--- src/main/java/org/reactome/web/nursa/client/details/tabs/dataset/widgets/AnalysisPanel.java
+++ src/main/java/org/reactome/web/nursa/client/details/tabs/dataset/widgets/AnalysisPanel.java
@@ -1,6 +1,5 @@
 package org.reactome.web.nursa.client.details.tabs.dataset.widgets;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
@@ -29,6 +28,7 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DoubleBox;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RadioButton;
@@ -38,13 +38,16 @@ import com.google.gwt.user.client.ui.Widget;
 /**
  * @author Fred Loney <loneyf@ohsu.edu>
  */
-public abstract class PathwayPanel extends Composite {
+public abstract class AnalysisPanel extends Composite {
     
+    private static final double DEF_BINOMIAL_CUTOFF = .05;
+
     /**
      * The UiBinder interface.
      */
-    interface Binder extends UiBinder<Widget, PathwayPanel> {
+    interface Binder extends UiBinder<Widget, AnalysisPanel> {
     }
+    
     static final Binder uiBinder = GWT.create(Binder.class);
 
     final String GENE_NAMES_HEADER = "#Gene names";
@@ -56,16 +59,16 @@ public abstract class PathwayPanel extends Composite {
     Panel content;
     
     /**
-     * The GSEA analysis radio button.
-     */
-    @UiField()
-    RadioButton gseaBtn;
-    
-    /**
      * The binomial analysis radio button.
      */
     @UiField()
     RadioButton binomialBtn;
+    
+    /**
+     * The GSEA analysis radio button.
+     */
+    @UiField()
+    RadioButton gseaBtn;
 
     /**
      * The config slider button.
@@ -86,16 +89,34 @@ public abstract class PathwayPanel extends Composite {
     Label runningLbl;
 
     /**
-     * The config slider button.
+     * The configuration button.
      */
     @UiField()
-    Button sliderBtn;
+    Button configBtn;
 
     /**
      * The config slider container.
      */
     @UiField()
-    SimplePanel sliderPanel;
+    Widget configPanel;
+
+    /**
+     * The binomial config container.
+     */
+    @UiField()
+    Widget binomialConfig;
+
+    /**
+     * The binomial p-value cut-off.
+     */
+    @UiField()
+    DoubleBox binomialCutoff;
+
+    /**
+     * The GSEA config slider container.
+     */
+    @UiField()
+    SimplePanel gseaConfig;
 
     /**
      * The analysis result display.
@@ -107,7 +128,7 @@ public abstract class PathwayPanel extends Composite {
 
     protected EventBus eventBus;
 
-    public PathwayPanel(EventBus eventBus) {
+    public AnalysisPanel(EventBus eventBus) {
         this.eventBus = eventBus;
         initWidget(uiBinder.createAndBindUi(this));
         // The configuration control panel.
@@ -125,7 +146,7 @@ public abstract class PathwayPanel extends Composite {
         List<List<String>> rankedList =
                 experiment.getDataPoints()
                            .stream()
-                           .map(PathwayPanel::pullRank)
+                           .map(AnalysisPanel::pullRank)
                            .collect(Collectors.toList());
         // Obtain the dataset size parameters.
         int[] dataSetBounds = gseaConfigSlider.getValues();
@@ -152,9 +173,12 @@ public abstract class PathwayPanel extends Composite {
 
     protected void binomialAnalyse(Experiment experiment, Consumer<AnalysisResult> consumer) {
         // The input is a table of gene symbol lines.
+        // The input data points are filtered by the cut-off value.
+        double cutoff = binomialCutoff.getValue();
         List<String> rankedList =
                 experiment.getDataPoints()
                           .stream()
+                          .filter(dp -> dp.getPvalue() <= cutoff)
                           .map(dp -> dp.getSymbol())
                           .collect(Collectors.toList());
         rankedList.add(0, GENE_NAMES_HEADER);
@@ -200,44 +224,10 @@ public abstract class PathwayPanel extends Composite {
 
     private void buildConfigPanel() {
         content.addStyleName(RESOURCES.getCSS().main());
-        // The sliderBtn comment below regarding style
-        // applies to launchPad and runningLbl as well.
-        launchPad.addStyleName(RESOURCES.getCSS().launchPad());
-        runningLbl.addStyleName(RESOURCES.getCSS().running());
-        runningLbl.setVisible(false);
-        
-        launchBtn.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                launchBtn.setVisible(false);
-                runningLbl.setVisible(true);
-                if (gseaBtn.getValue()) {
-                    gseaAnalyse();
-                } else {
-                    binomialAnalyse();
-                }
-            }
-        });
-
-        // For now, the slider panel only applies to GSEA.
-        // Per the RadioButton javadoc, the value change event is only
-        // triggered when the button is clicked, not when it is cleared,
-        // but it is good form to check the value here anyway.
-        gseaBtn.setValue(true);
-        gseaBtn.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
-
-            @Override
-            public void onValueChange(ValueChangeEvent<Boolean> event) {
-                if (event.getValue()) {
-                    sliderBtn.setVisible(true);
-                }
-            }
-
-        });
-        
-        // Note: it would be preferable to set the style in PathwayPanel.ui.xml:
-        //     <ui:style src="PathwayPanel.css" />
-        // and then add the styles to the corresponding widgets, e.g:
+        // Note: it would be preferable to set the style for the
+        // widgets below in AnalysisPanel.ui.xml, e.g.:
+        //     <ui:style src="AnalysisPanel.css" />
+        //     ...
         //     <g:Button ui:field='sliderBtn' addStyleNames="{style.sliderBtn}">
         // GWT and SO snippets suggest this is feasible and recommended, e.g.
         // https://stackoverflow.com/questions/1899007/add-class-name-to-element-in-uibinder-xml-file
@@ -247,32 +237,81 @@ public abstract class PathwayPanel extends Composite {
         //   this.get_clientBundleFieldNameUnlikelyToCollideWithUserSpecifiedFieldOkay_4_g$(...).style_19_g$ is not a function
         // The work-around is to set the style the old-fashioned verbose way with
         // the CSS resource below.
-        sliderBtn.addStyleName(RESOURCES.getCSS().slider());
-        sliderBtn.setVisible(gseaBtn.getValue());
+        launchPad.addStyleName(RESOURCES.getCSS().launchPad());
+        launchBtn.addStyleName(RESOURCES.getCSS().launchBtn());
+        gseaBtn.addStyleName(RESOURCES.getCSS().gseaBtn());
+        runningLbl.addStyleName(RESOURCES.getCSS().running());
+        configBtn.addStyleName(RESOURCES.getCSS().configBtn());
+        configPanel.addStyleName(RESOURCES.getCSS().configPanel());
+        binomialCutoff.addStyleName(RESOURCES.getCSS().binomialCutoff());
+        
+        // Initialize the binomial cut-off.
+        binomialCutoff.setValue(DEF_BINOMIAL_CUTOFF);
+        
+        // Make the slider.
         gseaConfigSlider = new GseaConfigSlider();
-        sliderPanel.addStyleName("gsea-config");
-        sliderPanel.getElement().appendChild(gseaConfigSlider.getElement());
-        sliderPanel.setVisible(false);
-        // The slider button toggles the slider panel display.
-        sliderBtn.addClickHandler(new ClickHandler() {
+        // The GSEA config style must be defined in the global
+        // style.css file since it uses the third-party slider
+        // widget styles.
+        gseaConfig.addStyleName("gsea-config");
+        gseaConfig.getElement().appendChild(gseaConfigSlider.getElement());
+        // The config panel is displayed when the config button
+        // is clicked.
+        configPanel.setVisible(false);
+        // The config button toggles the config panel display.
+        configBtn.addClickHandler(new ClickHandler() {
 
             @Override
             public void onClick(ClickEvent event) {
-                sliderPanel.setVisible(!sliderPanel.isVisible());
+                configPanel.setVisible(!configPanel.isVisible());
             }
 
         });
-
+        
+        // Per the RadioButton javadoc, the value change event is only
+        // triggered when the button is clicked, not when it is cleared,
+        // but it is good form to check the value here anyway.
         binomialBtn.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
 
             @Override
             public void onValueChange(ValueChangeEvent<Boolean> event) {
                 if (event.getValue()) {
-                    sliderBtn.setVisible(false);
-                    sliderPanel.setVisible(false);
+                    binomialConfig.setVisible(true);
+                    gseaConfig.setVisible(false);
                 }
             }
 
+        });
+        
+        // The default config is binomial.
+        binomialBtn.setValue(true);
+        gseaConfig.setVisible(false);
+        
+        gseaBtn.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
+
+            @Override
+            public void onValueChange(ValueChangeEvent<Boolean> event) {
+                if (event.getValue()) {
+                    gseaConfig.setVisible(true);
+                    binomialConfig.setVisible(false);
+                }
+            }
+
+        });
+        
+        runningLbl.setVisible(false);
+        launchBtn.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                launchBtn.setVisible(false);
+                runningLbl.setVisible(true);
+                analysisPanel.clear();
+                if (gseaBtn.getValue()) {
+                    gseaAnalyse();
+                } else {
+                    binomialAnalyse();
+                }
+            }
         });
     }
 
@@ -328,15 +367,23 @@ public abstract class PathwayPanel extends Composite {
         /**
          * The path to the default CSS styles used by this resource.
          */
-        String CSS = "PathwayPanel.css";
+        String CSS = "AnalysisPanel.css";
 
         String main();
 
+        String gseaBtn();
+
         String launchPad();
+
+        String launchBtn();
 
         String running();
 
-        String slider();
+        String configBtn();
+
+        String configPanel();
+
+        String binomialCutoff();
 
     }
 }
