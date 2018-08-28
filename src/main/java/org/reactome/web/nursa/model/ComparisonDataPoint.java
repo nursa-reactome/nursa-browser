903fe14cf5530f8de1b17c83fc334381478cf2ce
FredLoney <loneyf@ohsu.edu>
2018-08-28 10:46:48 -0700

    Add fold change difference column.

--- src/main/java/org/reactome/web/nursa/model/ComparisonDataPoint.java
+++ src/main/java/org/reactome/web/nursa/model/ComparisonDataPoint.java
@@ -27,15 +27,26 @@ public class ComparisonDataPoint {
     /**
      * @return the log10(pvalue1/pvalue2) ratio
      */
-    public Double getRatio() {
+    public Double getPValueRatio() {
         DataPoint dp1 = getDataPoints()[0];
         Double value1 = getPvalue(dp1);
         DataPoint dp2 = getDataPoints()[1];
         Double value2 = getPvalue(dp2);
         return value1 == null || value2 == null ? null : Math.log10(value1 / value2);
     }
+    
+    /**
+     * @return FC1 - FC2
+     */
+    public Double getFCDifference() {
+        DataPoint dp1 = getDataPoints()[0];
+        Double fc1 = dp1 == null ? null : dp1.getFoldChange();
+        DataPoint dp2 = getDataPoints()[1];
+        Double fc2 = dp2 == null ? null : dp2.getFoldChange();
+        return fc1 == null || fc2 == null ? null : fc2 - fc1;
+    }
 
-    protected Double getPvalue(DataPoint dp) {
+    private Double getPvalue(DataPoint dp) {
         if (dp == null) {
              return null;
         }
