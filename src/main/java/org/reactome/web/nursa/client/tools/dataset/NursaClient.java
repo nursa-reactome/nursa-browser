50ab1de77d068f2c6945ee031059242757015f9c
FredLoney <loneyf@ohsu.edu>
2019-05-10 09:32:22 -0700

    Add experimentId REST parameter.

--- src/main/java/org/reactome/web/nursa/client/tools/dataset/NursaClient.java
+++ src/main/java/org/reactome/web/nursa/client/tools/dataset/NursaClient.java
@@ -8,10 +8,9 @@ import javax.ws.rs.QueryParam;
 
 import org.fusesource.restygwt.client.MethodCallback;
 import org.fusesource.restygwt.client.RestService;
-import org.reactome.nursa.model.DataPoint;
 import org.reactome.nursa.model.DataSet;
 import org.reactome.nursa.model.DataSetSearchResult;
-import org.reactome.nursa.model.Experiment;
+import org.reactome.nursa.model.DisplayableDataPoint;
 
 
 @Path("/NursaService")
@@ -27,8 +26,8 @@ public interface NursaClient extends RestService {
     @GET
     @Path("/datapoints")
     public void getDataPoints(@QueryParam("doi") String doi,
-                           @QueryParam("experimentNumber") int expNbr,
-                           MethodCallback<List<DataPoint>> dataset);
+                           @QueryParam("experimentId") int experimentId,
+                           MethodCallback<List<DisplayableDataPoint>> dataset);
 
     @GET
     @Path("/search")
