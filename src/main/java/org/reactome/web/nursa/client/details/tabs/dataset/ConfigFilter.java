6957693fadc1668e73d9f8cd3faa4cf13100178b
FredLoney <loneyf@ohsu.edu>
2019-06-04 15:04:54 -0700

    Correctly handle missing config filter cutoff values.

--- src/main/java/org/reactome/web/nursa/client/details/tabs/dataset/ConfigFilter.java
+++ src/main/java/org/reactome/web/nursa/client/details/tabs/dataset/ConfigFilter.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiPredicate;
 
+import org.reactome.web.fireworks.util.Console;
+
 public class ConfigFilter {
     
     public static enum Conjunction {
@@ -16,46 +18,53 @@ public class ConfigFilter {
     private ConfigFilter other;
     private Conjunction conjunction;
 
-    private ConfigFilter(Double[] cutoffs, List<BiPredicate<Double, Double>> comparators,
-            Conjunction[] conjunctions, int index) {
-        while (cutoffs[index] == null && index < cutoffs.length) {
-            index++;
-        }
-        if (index == cutoffs.length) {
-            // The trivial filter.
-            this.comparator = (value, cutoff) -> true;
-        } else {
-            this.cutoff = cutoffs[index];
-            while (cutoffs[index] == null && index < cutoffs.length) {
-                index++;
-            }
-            if (index == cutoffs.length - 1) {
-                this.comparator = comparators.get(index);
-            } else {
-                this.other = new ConfigFilter(cutoffs, comparators, conjunctions, index + 1);
-                this.comparator = comparators.get(index);
-                this.conjunction = conjunctions[index];
-            }
-        }
-    }
-
+    /**
+     * Creates a value array filter.
+     * 
+     * @param cutoffs the N values to compare against
+     * @param comparators the N value/cutofff comparison predicates
+     * @param conjunctions the N-1 AND/OR compound filter logical operations
+     */
     public ConfigFilter(Double[] cutoffs, List<BiPredicate<Double, Double>> comparators,
             Conjunction[] conjunctions) {
         this(cutoffs, comparators, conjunctions, 0);
     }
+
+    private ConfigFilter(Double[] cutoffs, List<BiPredicate<Double, Double>> comparators,
+            Conjunction[] conjunctions, int index) {
+        cutoff = cutoffs[index];
+        if (cutoff != null) {
+            comparator = comparators.get(index);
+        }
+        if (index < cutoffs.length) {
+            // Make another filter for the remaining cutoffs.
+            other = new ConfigFilter(cutoffs, comparators, conjunctions, index + 1);
+            // AND/OR the current comparison with the other filter.
+            if (cutoff != null) {
+                conjunction = conjunctions[index];
+            }
+        }
+    }
     
     public boolean test(Double ...values) {
+        // The remaining values to test.
+        Double[] rest = Arrays.copyOfRange(values, 1, values.length);
+        // A missing cutoff always passes the test.
+        if (cutoff == null) {
+            return other == null || other.test(rest);
+        }
         boolean result = comparator.test(values[0], cutoff);
-        if (this.other == null) {
+        if (other == null) {
             return result;
         }
-        Double[] rest = Arrays.copyOfRange(values, 1, values.length);
+        // AND/OR the first test result against the other filter result.
         switch (this.conjunction) {
         case AND:
             return result && other.test(rest);
         case OR:
             return result || other.test(rest);
         default:
+            // Should never occur by construction.
             String msg = "Unrecognized conjunction: " + this.conjunction;
             throw new IllegalArgumentException(msg);
         }
